370d254c9efd35dea0a5b0f3fb01b5f24274583e
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-12-19 17:48:26 -0800

    Changed knexfile.

--- knexfile.js
+++ knexfile.js
@@ -12,18 +12,9 @@ module.exports = {
         conn.run("PRAGMA foreign_keys = ON", done);
       }
     },
-
-    testing: {
-      client: "pg",
-      connection: process.env.DATABASE_URL,
-      migrations: {
-        directory: "./database/migrations"
-      },
-      seeds: { directory: "./database/seeds" }
-    },
-
     production: {
       client: "pg",
+      useNullAsDefault: true,
       connection: process.env.DATABASE_URL,
       migrations: {
         directory: "./database/migrations"
