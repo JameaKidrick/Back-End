3a2bc6c00213314f8d740943fb10c5b2d68b1ea0
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-12-19 21:10:58 -0800

    Changed index.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const server = require("./api/server.js");
+require("dotenv").config();
+
+const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on port: ${port} *\n`);
+});
