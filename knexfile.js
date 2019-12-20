b2d3cb86fd6d933e7e05d7a332d4ca7d28c25f54
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-12-19 18:31:17 -0800

    Added some jokes-router and jokes-model. Changed knexfile.

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     client: "pg",
     useNullAsDefault: true,
-    connection: process.env.DATABASE_URL,
+    connection: {
+      filename: "./database/dadjokes.dbu"
+    },
     migrations: {
       directory: "./database/migrations"
     },
