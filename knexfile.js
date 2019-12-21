b6acfce342120c2fc14ab42488effe7f3f91c84d
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-12-20 21:42:19 -0500

    unwrapped production from development environment

--- knexfile.js
+++ knexfile.js
@@ -15,15 +15,15 @@ module.exports = {
       afterCreate: (conn, done) => {
         conn.run("PRAGMA foreign_keys = ON", done);
       }
-    },
-    production: {
-      client: "pg",
-      useNullAsDefault: true,
-      connection: process.env.DATABASE_URL,
-      migrations: {
-        directory: "./database/migrations"
-      },
-      seeds: { directory: "./database/seeds" }
     }
+  },
+  production: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: { directory: "./database/seeds" }
   }
 };
